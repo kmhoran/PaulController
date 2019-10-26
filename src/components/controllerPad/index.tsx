66a819cdce6b4bd9d38241c1966e0b5872c5c65c
kmhoran <kevin.michael.horan@gmail.com>
2019-10-26 08:07:07 -0700

    give up on controllerpad limiter -- just stick with oversflow hidden

--- src/components/controllerPad/index.tsx
+++ src/components/controllerPad/index.tsx
@@ -31,13 +31,10 @@ class ControllerPad extends Component<IProps, IState> {
         });
         this.animatedValue.setValue({ x: 0, y: 0 });
       },
-      onPanResponderMove: (e, gestureState) => {
-        this.getMaxPadDistance();
-        Animated.event([
-          null,
-          { dx: this.animatedValue.x, dy: this.animatedValue.y }
-        ])(e, gestureState);
-      },
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: this.animatedValue.x, dy: this.animatedValue.y }
+      ]),
       onPanResponderRelease: (e, gestureState) => {
         this.animatedValue.flattenOffset();
         Animated.spring(this.animatedValue, {
@@ -49,31 +46,6 @@ class ControllerPad extends Component<IProps, IState> {
     });
   }
 
-  getMaxPadDistance() {
-    // console.log(state);
-    const MAX_DIST = 100 - 50;
-    //   const x = this._value.x;
-    const x = this.animatedValue.x._value;
-    const y = this.animatedValue.y._value;
-    // console.log("X:", x);
-    // console.log("Y:", y);
-    const rSlope = (Math.atan2(this._value.y, x));
-    console.log("SLOPE:", rSlope);
-    const maxX = MAX_DIST * Math.cos(rSlope);
-    const maxY = MAX_DIST * Math.sin(rSlope);
-    const normMaxX = Math.abs(maxX);
-    const normMaxY = Math.abs(maxY);
-    const normX = Math.abs(x);
-    const normY = Math.abs(y)
-    console.log(normX>normMaxX? `X: too far ${normX} > ${normMaxX}`: `X: ok ${normX} < ${normMaxX}`);
-    console.log(normY>normMaxY? `Y: too far ${normY} > ${normMaxY}`: `Y: ok ${normY} < ${normMaxY}`);
-    if(normX>maxX){
-        this.animatedValue.setValue({
-            x: maxX,
-            y: maxY
-        })
-    }
-  }
   render() {
     const animatedStyle = {
       transform: this.animatedValue.getTranslateTransform()
@@ -98,7 +70,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#555",
     justifyContent: "center",
     borderWidth: 5,
-    borderColor: "#222"
+    borderColor: "#222",
+    overflow: "hidden"
   },
   padCenter: {
     height: 100,
